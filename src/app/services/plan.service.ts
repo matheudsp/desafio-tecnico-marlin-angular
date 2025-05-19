da100fef21bc8058d41958e8b01992f5947354b6
mdsp <mdsp.personal@gmail.com>
2025-05-19 08:20:44 -0300

    fix(plans): fixed INVALID_ULR error from AXIOS REQUEST METHOD GET

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { Plan } from '../models/plan';
 import axios from 'axios';
+import plans from '../../assets/plans.json'; //caminho local
 @Injectable({
   providedIn: 'root',
 })
 export class PlanService {
-  private url = 'assets/plans.json'; //caminho local
-
-  constructor() {}
-
   getPlans(): Observable<Plan[]> {
     // Usando AXIOS para receber os planos
-    // Converte PROMISE para OBSERVABLE c/ operador 'from' 
-    return from(
-      axios
-        .get<Plan[]>(this.url)
-        .then((response: { data: Plan[] }) => response.data)
-    );
+    // Converte PROMISE para OBSERVABLE c/ operador 'from'
+    return of(plans);
   }
 }
